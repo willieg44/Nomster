df9547d10f986af7c41dda177b85aa11e9e79e07
willieg44@gmail.com <willieg44@gmail.com>
2019-10-02 00:59:17 +0000

    add create box

--- Gemfile
+++ Gemfile
@@ -67,3 +67,10 @@ gem 'bootstrap', '4.0.0.alpha6'
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
+## Gemfile for Rails 3+
+gem 'will_paginate', '~> 3.1.1'
+gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
+gem 'will_paginate-bootstrap4', '~> 0.2.2'
+ 
+#this is needed to add bootsrap style to pagination
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,25 @@
 class PlacesController < ApplicationController
-
-
-
   def index
-    @places = Place.all
+    @places = Place.paginate(:page => params[:page], :per_page => 2)
+  end 
+
+  def new
+    @place = Place.new
+  end 
+
+  def create
+    PLace.create(place_params)
+    redirect_to root_path
+  end
+  
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
   end  
+
+
+
 end
+
+
