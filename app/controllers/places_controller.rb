4527a0edf35bafaa8290f9c44ba68e6b5df6e714
willieg44@gmail.com <willieg44@gmail.com>
2019-10-09 02:27:01 +0000

    made sure not logged in users can delete places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 2)
   end 
@@ -20,12 +20,15 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
-  
   def update
     @place = Place.find(params[:id])
     @place.update_attributes(place_params)
-    redirct_to root_path
+    redirect_to root_path
   end
 
   def destroy
