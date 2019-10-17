f19ebacd1040beacab63a18479cbb87e51e274c0
willieg44@gmail.com <willieg44@gmail.com>
2019-10-17 00:30:07 +0000

    updated sending emails with comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    after_create :send_comment_email
 
     RATINGS = {
         'one star': '1_star',
@@ -10,7 +11,12 @@ class Comment < ApplicationRecord
         'five stars': '5_stars'
     }
 
-        def humanized_rating
-            RATINGS.invert[self.rating]
-        end
+def humanized_rating
+    RATINGS.invert[self.rating]
+end
+
+def send_comment_email
+    NotificationMailer.comment_added(self).deliver_now
+end
+
 end
