c2c3aef0a873061550dc7a6a4f94cd12e784c5d1
willieg44@gmail.com <willieg44@gmail.com>
2019-10-22 02:12:15 +0000

    added live email notifications

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,10 @@
 class Photo < ApplicationRecord
     belongs_to :user
     belongs_to :place
+
+
+
+    def humanized_rating
+        CAPTIONS.invert[self.rating]
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_003158) do
+ActiveRecord::Schema.define(version: 2019_10_21_231624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_10_17_003158) do
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"
+    t.integer "user_id"
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
