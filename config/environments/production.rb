745c6e25c4bcb7b41eb9937ac4341dba6acd5263
willieg44@gmail.com <willieg44@gmail.com>
2019-10-27 18:51:09 +0000

    pushing emails to live host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
+end
   config.action_mailer.default_url_options = { host: 'nomster-will-gillam.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
