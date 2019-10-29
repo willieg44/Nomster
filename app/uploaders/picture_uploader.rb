015fb87d439796049bb7a702655c28e3c7f238c3
willieg44@gmail.com <willieg44@gmail.com>
2019-10-29 17:24:12 +0000

    trying to get pictures to work

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+
   if Rails.env.production?
     storage :fog
   else
